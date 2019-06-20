a4ff3a5b4c9722cddea55b5943b4ad23b13aa6dc
tom110 <254449149@qq.com>
2019-06-20 16:08:51 +0800

    初次提交

---
+++ ini/IniParser.py
@@ -0,0 +1,14 @@
+import configparser
+import os
+
+class IniParser:
+    def __init__(self,file):
+        cf = configparser.ConfigParser()
+        cf.read(os.getcwd() + file, encoding="utf-8")
+        try:
+            self.vote_url = cf.get('vote','vote_url')
+            self.vote_getproxyurl = cf.get('vote','vote_getproxyurl')
+            self.vote_number = cf.get('vote', 'vote_number')
+        except configparser.NoOptionError:
+            print('不能读取配置文件！')
+            os.exit(1)
---
+++ mutiprocess.py
@@ -0,0 +1,119 @@
+# -*- coding: UTF-8 -*-
+from multiprocessing import Process, Queue
+import multiprocessing
+import os, time
+from urllib import request
+import json
+from ini.IniParser import IniParser
+
+iniParser = IniParser("/ini/config.ini")
+vote_url=iniParser.vote_url
+vote_getproxyurl=iniParser.vote_getproxyurl
+vote_number=int(iniParser.vote_number)
+
+def getproxy():
+    url=vote_getproxyurl
+    req = request.Request(url)
+    req.add_header("User-Agent",
+                   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3080.5 Safari/537.36")
+    try:
+        response = request.urlopen(req, timeout=10)
+        return response.read().decode("utf-8")
+    except:
+        print( "proxy获取失败")
+        return ""
+
+# 递归访问投票
+def gorequest(req,proxy,i,count):
+    res=0
+    try:
+        if count.value >= vote_number:
+            print('投票任务完成，%d进程停止运行' % i)
+            kill(os.getpid())
+        else:
+            response = request.urlopen(req, timeout=10)
+            data = json.loads(response.read().decode("utf-8"))
+            res=data["result"]
+    except:
+        print("进程%d:%s失效" % (i,proxy))
+        pass
+
+    if res == 0:
+        print("进程%d,%s达到投票上限" % (i,proxy))
+        pass
+    elif res==1:
+        count.value = count.value + 1
+        print("进程%d号，代理%s成功投票,累记投票%d张" % (i,proxy,count.value))
+        gorequest(req,proxy,i,count)
+    else:
+        print(proxy+"未知错误")
+        pass
+
+# 处理消费事件
+def exec(proxy,i,count):
+    url = vote_url
+
+    req = request.Request(url)
+    req.set_proxy(proxy, 'http')
+
+    req.add_header("User-Agent",
+                   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3080.5 Safari/537.36")
+
+    gorequest(req,proxy,i,count)
+
+# 写数据进程执行的代码:
+def write(q):
+    print('进程%s开始写入代理池' % os.getpid())
+    while True:
+        if q.qsize()>10:
+            time.sleep(5)
+            print('队列过长，代理线程池停止5秒')
+        else:
+            proxy=getproxy()
+            print('代理地址%s写入代理池' % proxy)
+            q.put(proxy)
+            time.sleep(0.5)
+
+# 读数据进程执行的代码:
+def read(q,i,count):
+    print('进程%s加入投票序列，进程编号%d' % (os.getpid(),i))
+    while True:
+        value = q.get(True)
+        exec(value,i,count)
+
+# 停止进程
+def kill(pid):
+    # 本函数用于中止传入pid所对应的进程
+    if os.name == 'nt':
+        # Windows系统
+        cmd = 'taskkill /pid ' + str(pid) + ' /f'
+        try:
+            os.system(cmd)
+            # print('%s进程已被停止'% pid)
+        except Exception as e:
+            print(e)
+    elif os.name == 'posix':
+        # Linux系统
+        cmd = 'kill ' + str(pid)
+        try:
+            os.system(cmd)
+            # print('%s进程已被停止' % pid)
+        except Exception as e:
+            print(e)
+    else:
+        print('Undefined os.name')
+
+
+if __name__=='__main__':
+    count=multiprocessing.Value("d",0)
+    # 父进程创建Queue，并传给各个子进程：
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pw.start()
+
+    for i in range(11):
+        pr=Process(target=read,args=(q,i,count))
+        pr.start()
+
+    pw.join()
+    pw.close()
